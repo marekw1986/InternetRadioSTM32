ea1aa1a1453defd7798570198ab0f36c56a673d0
marekw1986 <marekw1986@gmail.com>
2022-09-27 12:48:32 +0200

    Ring buffer in SPI RAM (in progress)

--- code/Core/Src/spiram.c
+++ code/Core/Src/spiram.c
@@ -24,6 +24,9 @@
 
 extern SPI_HandleTypeDef hspi3;
 
+static uint32_t spiram_ringbuffer_head = 0;
+static uint32_t spiram_ringbuffer_tail = 0;
+
 static void spiram_setmode(uint8_t mode ) {
 	uint8_t tmp[2] = {WRMR, mode};
 
@@ -37,6 +40,8 @@ void spiram_clear(void) {
 	uint8_t tmp[4] = {WRITE, 0x00, 0x00, 0x00};
 	int i;
 
+	spiram_ringbuffer_head = 0;
+	spiram_ringbuffer_tail = 0;
 	spiram_setmode(SEQ_MODE);
 
 	enableRAM();
@@ -95,4 +100,58 @@ void spiram_readarray(uint32_t address, uint8_t *data, uint16_t len) {
 	HAL_SPI_Transmit(&hspi3, tmp, 4, HAL_MAX_DELAY);
 	HAL_SPI_Receive(&hspi3, data, len, HAL_MAX_DELAY);
 	disableRAM();
+}
+
+
+void spiram_write_array_to_ringbuffer(uint8_t* data, uint16_t len) {
+	uint8_t tmp[4] = {WRITE, (uint8_t)(spiram_ringbuffer_head >> 16), (uint8_t)(spiram_ringbuffer_head >> 8), (uint8_t)spiram_ringbuffer_head};
+	spiram_setmode(SEQ_MODE);
+
+	enableRAM();
+	HAL_SPI_Transmit(&hspi3, tmp, 4, HAL_MAX_DELAY);
+	for (int i=0; i<len; i++) {
+		HAL_SPI_Transmit(&hspi3, &data[i], 1, HAL_MAX_DELAY);
+		spiram_ringbuffer_head++;
+		if (spiram_ringbuffer_head > 0x20000 /*end of buffer*/) {
+			spiram_ringbuffer_head = 0;
+			disableRAM();
+			//delay?
+			tmp[0] = WRITE;
+			tmp[1] = (uint8_t)(spiram_ringbuffer_head >> 16);
+			tmp[2] = (uint8_t)(spiram_ringbuffer_head >> 8);
+			tmp[3] = (uint8_t)spiram_ringbuffer_head;
+			spiram_setmode(SEQ_MODE);
+			enableRAM();
+			HAL_SPI_Transmit(&hspi3, tmp, 4, HAL_MAX_DELAY);
+		}
+	}
+	disableRAM();
+}
+
+uint32_t read_array_from_ringbuffer(uint8_t* data, uint32_t len) {
+	uint8_t tmp[4] = {READ, (uint8_t)(spiram_ringbuffer_tail >> 16), (uint8_t)(spiram_ringbuffer_tail >> 8), (uint8_t)spiram_ringbuffer_tail};
+	uint32_t bytes_read = 0;
+
+	enableRAM();
+	HAL_SPI_Transmit(&hspi3, tmp, 4, HAL_MAX_DELAY);
+	for(int i=0; i<len; i++) {
+		HAL_SPI_Receive(&hspi3, &data[i], 1, HAL_MAX_DELAY);
+		spiram_ringbuffer_tail++;
+		bytes_read++;
+		if (spiram_ringbuffer_tail > 0x20000) {
+			spiram_ringbuffer_tail = 0;
+			disableRAM();
+			//delay?
+			tmp[0] = READ;
+			tmp[1] = (uint8_t)(spiram_ringbuffer_tail >> 16);
+			tmp[2] = (uint8_t)(spiram_ringbuffer_tail >> 8);
+			tmp[3] = (uint8_t)spiram_ringbuffer_tail;
+			spiram_setmode(SEQ_MODE);
+			enableRAM();
+			HAL_SPI_Transmit(&hspi3, tmp, 4, HAL_MAX_DELAY);
+		}
+	}
+	disableRAM();
+
+	return bytes_read;
 }
